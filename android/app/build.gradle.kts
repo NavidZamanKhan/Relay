4e1d1c2795d94bec29117c0602aad5df76ce7c5a
Navid Zaman Khan <navid166418@gmail.com>
2026-09-23 01:45:25 +0600

    build: configure Android desugaring and macOS release targets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,10 +9,11 @@ plugins {
 
 android {
     namespace = "com.navidzamankhan.relay"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -41,6 +42,10 @@ android {
     }
 }
 
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
@@ -50,3 +55,4 @@ kotlin {
 flutter {
     source = "../.."
 }
+
